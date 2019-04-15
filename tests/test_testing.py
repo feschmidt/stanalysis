b8f1aac73986bb3ace5906e2370a1a8a9fd9f38c
mgely <mario.f.gely@gmail.com>
2019-04-15 14:42:19 +0200

    dummy testing

---
+++ tests/test_testing.py
@@ -0,0 +1,11 @@
+import unittest
+
+class TestTesting(unittest.TestCase):
+
+    def test_testing(self):
+        # Do some stuff
+        self.assertEqual(1,0)
+
+    def test_testing_2(self):
+        # Do some stuff
+        self.assertEqual(1,1)
