267dd88a752ee899e5706aea375e00a12058ab8d
mgely <mario.f.gely@gmail.com>
2019-04-15 14:42:52 +0200

    small change

--- tests/test_testing.py
+++ tests/test_testing.py
@@ -1,11 +1,17 @@
 import unittest
+import stanalysis
 
 class TestTesting(unittest.TestCase):
 
     def test_testing(self):
-        # Do some stuff
+        # Do some stuff (with stanalysis)
         self.assertEqual(1,0)
 
     def test_testing_2(self):
-        # Do some stuff
+        # Do some stuff (with stanalysis)
         self.assertEqual(1,1)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
