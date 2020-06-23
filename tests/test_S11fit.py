60e1b0e6c6048d2808477bc4a8f9d79320d3c941
Mario Gely - TNW <mfgely@tudelft.net>
2020-06-23 16:17:31 +0200

    testing travis

--- tests/test_S11fit.py
+++ tests/test_S11fit.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 import unittest
 import stlabutils
 import numpy as np
-
+ 
 
 class TestS11fit(unittest.TestCase):
     def test_S11fit_f0(self):
